47a9b5fa7aafa67d01d6d6271f02e691620f8050
Dmitri Don <fotkin@gmail.com>
2019-07-24 19:29:33 +0300

    CORS fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: SvelteWP
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Fotki Agency / Dmitri Don
  * Text Domain: sveltewp
  * Domain Path: /languages
@@ -27,6 +27,10 @@ function get_menu() {
     //return wp_get_nav_menu_items('menu');
     // return wp_get_nav_menu_items('menu');
 }
+
+add_action('init', function () {
+    header("Access-Control-Allow-Origin: *");
+});
 
 add_action('rest_api_init', function () {
     register_rest_route('myroutes', '/menu', [
